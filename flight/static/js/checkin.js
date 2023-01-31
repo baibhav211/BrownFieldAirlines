5f492b75e49b109835c1f561d274fa1540c769ff
baibhav211 <baibhav211@gmail.com>
2023-02-01 00:53:29 +0530

    inititial

---
+++ flight/static/js/checkin.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('input[type="checkbox" name="proceed_checkin"]').setCustomValidity('Please review the details to be correct and check the box to proceed.');
+  });
